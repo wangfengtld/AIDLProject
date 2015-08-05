08f34480476ef2dd4244a389632456889edc3f82
wangfeng <wangfengtld@126.com>
2015-08-05 18:12:12 +0800

    多增加一个app，用于测试

---
+++ MyApplication/app/src/main/java/com/example/wangfeng/aidltest/IRemoteService.java
@@ -0,0 +1,201 @@
+/*
+ * This file is auto-generated.  DO NOT MODIFY.
+ * Original file: /Users/wangfeng/Documents/workspace/java/AIDLProject/AIDLTest/app/src/main/aidl/com/example/wangfeng/aidltest/IRemoteService.aidl
+ */
+package com.example.wangfeng.aidltest;
+// Declare any non-default types here with import statements
+
+public interface IRemoteService extends android.os.IInterface
+{
+/** Local-side IPC implementation stub class. */
+public static abstract class Stub extends android.os.Binder implements com.example.wangfeng.aidltest.IRemoteService
+{
+private static final String DESCRIPTOR = "com.example.wangfeng.aidltest.IRemoteService";
+/** Construct the stub at attach it to the interface. */
+public Stub()
+{
+this.attachInterface(this, DESCRIPTOR);
+}
+/**
+ * Cast an IBinder object into an com.example.wangfeng.aidltest.IRemoteService interface,
+ * generating a proxy if needed.
+ */
+public static com.example.wangfeng.aidltest.IRemoteService asInterface(android.os.IBinder obj)
+{
+if ((obj==null)) {
+return null;
+}
+android.os.IInterface iin = obj.queryLocalInterface(DESCRIPTOR);
+if (((iin!=null)&&(iin instanceof com.example.wangfeng.aidltest.IRemoteService))) {
+return ((com.example.wangfeng.aidltest.IRemoteService)iin);
+}
+return new com.example.wangfeng.aidltest.IRemoteService.Stub.Proxy(obj);
+}
+@Override public android.os.IBinder asBinder()
+{
+return this;
+}
+@Override public boolean onTransact(int code, android.os.Parcel data, android.os.Parcel reply, int flags) throws android.os.RemoteException
+{
+switch (code)
+{
+case INTERFACE_TRANSACTION:
+{
+reply.writeString(DESCRIPTOR);
+return true;
+}
+case TRANSACTION_registerParticipateCallback:
+{
+data.enforceInterface(DESCRIPTOR);
+com.example.wangfeng.aidltest.IParticipateCallback _arg0;
+_arg0 = com.example.wangfeng.aidltest.IParticipateCallback.Stub.asInterface(data.readStrongBinder());
+this.registerParticipateCallback(_arg0);
+reply.writeNoException();
+return true;
+}
+case TRANSACTION_unregisterParticipateCallback:
+{
+data.enforceInterface(DESCRIPTOR);
+com.example.wangfeng.aidltest.IParticipateCallback _arg0;
+_arg0 = com.example.wangfeng.aidltest.IParticipateCallback.Stub.asInterface(data.readStrongBinder());
+this.unregisterParticipateCallback(_arg0);
+reply.writeNoException();
+return true;
+}
+case TRANSACTION_join:
+{
+data.enforceInterface(DESCRIPTOR);
+android.os.IBinder _arg0;
+_arg0 = data.readStrongBinder();
+String _arg1;
+_arg1 = data.readString();
+this.join(_arg0, _arg1);
+reply.writeNoException();
+return true;
+}
+case TRANSACTION_leave:
+{
+data.enforceInterface(DESCRIPTOR);
+android.os.IBinder _arg0;
+_arg0 = data.readStrongBinder();
+this.leave(_arg0);
+reply.writeNoException();
+return true;
+}
+case TRANSACTION_getParticipators:
+{
+data.enforceInterface(DESCRIPTOR);
+java.util.List<String> _result = this.getParticipators();
+reply.writeNoException();
+reply.writeStringList(_result);
+return true;
+}
+}
+return super.onTransact(code, data, reply, flags);
+}
+private static class Proxy implements com.example.wangfeng.aidltest.IRemoteService
+{
+private android.os.IBinder mRemote;
+Proxy(android.os.IBinder remote)
+{
+mRemote = remote;
+}
+@Override public android.os.IBinder asBinder()
+{
+return mRemote;
+}
+public String getInterfaceDescriptor()
+{
+return DESCRIPTOR;
+}
+@Override public void registerParticipateCallback(com.example.wangfeng.aidltest.IParticipateCallback cb) throws android.os.RemoteException
+{
+android.os.Parcel _data = android.os.Parcel.obtain();
+android.os.Parcel _reply = android.os.Parcel.obtain();
+try {
+_data.writeInterfaceToken(DESCRIPTOR);
+_data.writeStrongBinder((((cb!=null))?(cb.asBinder()):(null)));
+mRemote.transact(Stub.TRANSACTION_registerParticipateCallback, _data, _reply, 0);
+_reply.readException();
+}
+finally {
+_reply.recycle();
+_data.recycle();
+}
+}
+@Override public void unregisterParticipateCallback(com.example.wangfeng.aidltest.IParticipateCallback cb) throws android.os.RemoteException
+{
+android.os.Parcel _data = android.os.Parcel.obtain();
+android.os.Parcel _reply = android.os.Parcel.obtain();
+try {
+_data.writeInterfaceToken(DESCRIPTOR);
+_data.writeStrongBinder((((cb!=null))?(cb.asBinder()):(null)));
+mRemote.transact(Stub.TRANSACTION_unregisterParticipateCallback, _data, _reply, 0);
+_reply.readException();
+}
+finally {
+_reply.recycle();
+_data.recycle();
+}
+}
+@Override public void join(android.os.IBinder token, String name) throws android.os.RemoteException
+{
+android.os.Parcel _data = android.os.Parcel.obtain();
+android.os.Parcel _reply = android.os.Parcel.obtain();
+try {
+_data.writeInterfaceToken(DESCRIPTOR);
+_data.writeStrongBinder(token);
+_data.writeString(name);
+mRemote.transact(Stub.TRANSACTION_join, _data, _reply, 0);
+_reply.readException();
+}
+finally {
+_reply.recycle();
+_data.recycle();
+}
+}
+@Override public void leave(android.os.IBinder token) throws android.os.RemoteException
+{
+android.os.Parcel _data = android.os.Parcel.obtain();
+android.os.Parcel _reply = android.os.Parcel.obtain();
+try {
+_data.writeInterfaceToken(DESCRIPTOR);
+_data.writeStrongBinder(token);
+mRemote.transact(Stub.TRANSACTION_leave, _data, _reply, 0);
+_reply.readException();
+}
+finally {
+_reply.recycle();
+_data.recycle();
+}
+}
+@Override public java.util.List<String> getParticipators() throws android.os.RemoteException
+{
+android.os.Parcel _data = android.os.Parcel.obtain();
+android.os.Parcel _reply = android.os.Parcel.obtain();
+java.util.List<String> _result;
+try {
+_data.writeInterfaceToken(DESCRIPTOR);
+mRemote.transact(Stub.TRANSACTION_getParticipators, _data, _reply, 0);
+_reply.readException();
+_result = _reply.createStringArrayList();
+}
+finally {
+_reply.recycle();
+_data.recycle();
+}
+return _result;
+}
+}
+static final int TRANSACTION_registerParticipateCallback = (android.os.IBinder.FIRST_CALL_TRANSACTION + 0);
+static final int TRANSACTION_unregisterParticipateCallback = (android.os.IBinder.FIRST_CALL_TRANSACTION + 1);
+static final int TRANSACTION_join = (android.os.IBinder.FIRST_CALL_TRANSACTION + 2);
+static final int TRANSACTION_leave = (android.os.IBinder.FIRST_CALL_TRANSACTION + 3);
+static final int TRANSACTION_getParticipators = (android.os.IBinder.FIRST_CALL_TRANSACTION + 4);
+}
+public void registerParticipateCallback(com.example.wangfeng.aidltest.IParticipateCallback cb) throws android.os.RemoteException;
+public void unregisterParticipateCallback(com.example.wangfeng.aidltest.IParticipateCallback cb) throws android.os.RemoteException;
+public void join(android.os.IBinder token, String name) throws android.os.RemoteException;
+public void leave(android.os.IBinder token) throws android.os.RemoteException;
+public java.util.List<String> getParticipators() throws android.os.RemoteException;
+}
---
+++ MyApplication/app/src/main/java/com/example/wangfeng/myapplication/MainActivity.java
@@ -0,0 +1,159 @@
+package com.example.wangfeng.myapplication;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Binder;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.RemoteException;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.wangfeng.aidltest.IParticipateCallback;
+import com.example.wangfeng.aidltest.IRemoteService;
+import com.example.wangfeng.myapplication.R;
+
+import java.util.logging.Handler;
+
+
+public class MainActivity extends Activity {
+
+  private Button bindBtn, unbindBtn, callBtn;
+  private ListView listView;
+  private ArrayAdapter mAdapter;
+  private IBinder mToken        = new Binder();
+  private boolean isBindService = false;
+  private boolean mIsJoin       = false;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    bindBtn = (Button) findViewById(R.id.bind);
+    unbindBtn = (Button) findViewById(R.id.unbind);
+    callBtn = (Button) findViewById(R.id.calll);
+    listView = (ListView) findViewById(R.id.listview);
+    mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+    listView.setAdapter(mAdapter);
+
+    bindBtn.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Intent intent = new Intent("com.example.wangfeng.aidltest.IRemoteService");
+//        intent.setAction("com.example.wangfeng.aidltest.IRemoteService");
+        isBindService = bindService(intent, mServiceConnection, BIND_AUTO_CREATE);
+      }
+    });
+    unbindBtn.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        if (isBindService) {
+          unbindService(mServiceConnection);
+          try {
+            mService.unregisterParticipateCallback(mParticipateCallback);
+          } catch (RemoteException e) {
+            e.printStackTrace();
+          }
+          isBindService = false;
+        }
+      }
+    });
+    callBtn.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        callRemote();
+      }
+    });
+  }
+
+  private IRemoteService mService;
+
+  private ServiceConnection mServiceConnection = new ServiceConnection() {
+    @Override
+    public void onServiceConnected(ComponentName name, IBinder service) {
+      Toast.makeText(MainActivity.this, "Service connected" + name, Toast.LENGTH_SHORT).show();
+      mService = IRemoteService.Stub.asInterface(service);
+      try {
+        mService.registerParticipateCallback(mParticipateCallback);
+      } catch (RemoteException e) {
+        e.printStackTrace();
+      }
+      isBindService = true;
+    }
+
+    @Override
+    public void onServiceDisconnected(ComponentName name) {
+      //特别注意，此方法只有service被后台杀死才会调用，主动调用unbindService不会调用此方法
+      Toast.makeText(MainActivity.this, "Service disconnected" + name, Toast.LENGTH_SHORT).show();
+      mService = null;
+      isBindService = false;
+    }
+  };
+
+  private void callRemote() {
+
+    if (mService == null) {
+      Toast.makeText(this, "Service is not available yet!", Toast.LENGTH_SHORT).show();
+      return;
+    }
+    if (!isBindService) {
+      Toast.makeText(this, "Service is unbind!", Toast.LENGTH_SHORT).show();
+      return;
+    }
+
+    try {
+      if (!mIsJoin) {
+        String name = "Client:" + "myapp" + 1;
+        mService.join(mToken, name);
+        int num = 10;
+        for (int i = 0; i < num; i++) {
+          IBinder iBinder  = new Binder();
+          final String nameClient = "clientmyapp:" + i;
+          mService.join(iBinder, nameClient);
+        }
+        callBtn.setText("leave");
+        mIsJoin = true;
+      } else {
+        mService.leave(mToken);
+        callBtn.setText("join");
+        mIsJoin = false;
+      }
+    } catch (RemoteException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private IParticipateCallback mParticipateCallback = new IParticipateCallback.Stub() {
+
+    @Override
+    public void onParticipate(String name, boolean joinOrLeave) throws RemoteException {
+      Log.d("message", "onParticipate  " + Thread.currentThread().getName());
+
+      Message message = handler.obtainMessage();
+      message.what = joinOrLeave ? 1 : 0;
+      message.obj = name;
+      handler.sendMessage(message);
+    }
+  };
+
+  private android.os.Handler handler = new android.os.Handler() {
+    @Override
+    public void handleMessage(Message msg) {
+      super.handleMessage(msg);
+      if (msg.what == 1) {
+        mAdapter.add(msg.obj);
+      } else {
+        mAdapter.remove(msg.obj);
+      }
+      listView.setSelection(mAdapter.getCount() - 1);
+    }
+  };
+}
